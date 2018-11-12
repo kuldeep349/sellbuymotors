d762e14f5bfa725d36b2d3e222b523b02086f561
349kuldeep <349kuldeep@gmail.com>
2018-11-12 10:05:44 +0530

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -83,66 +83,203 @@ app.get('/search-results', async function (req, res, next) {
             }, (error) => console.log(err));
 
 })
+
+//app.get('/auto-search-results', async function (req, res, next) {
+//    var q = url.parse(req.url, true).query;
+//    var path = 'https://www.autotrader.co.uk/results-car-search?radius=' + q.radius + '&make=' + q.make + '&model=' + q.model + '&aggregatedTrim=' + q.aggregatedTrim;
+//    axios.get(path)
+//            .then((response) => {
+//                if (response.status === 200) {
+//                    const html = response.data;
+//                    const $ = cheerio.load(html);
+//                    res.writeHead(200, {'Content-Type': 'application/json'});
+//                    var cars = {}, filters = {}, radius = {}, make = {}, model = {}, model_variant = {}, aggregatedTrim = {}
+//                    $(html.html).find('li.search-page__result').each(function (i, elem) {
+//                        cars[i] = {
+//                            product_id: $(this).attr('id'),
+//                            name: $(this).find('h2').text().trim(),
+//                            image: $(this).find(".listing-main-image .js-click-handler img").attr('src'),
+//                            listings: $(this).find(".listing-key-specs").html(),
+//                            title: $(this).find("p.listing-attention-grabber").text(),
+//                            description: $(this).find("p").text(),
+//                            price: $(this).find(".vehicle-price").text(),
+//                        }
+//                    });
+//                    $('<div>' + html.refinements.fields[0].html + '</div>').find(".value-button").each(function (i, elem) {
+//                        radius[i] = {
+//                            value: $(this).data('selected-value'),
+//                            text: $(this).data('selected-display-name'),
+//                        }
+//                    });
+//                    $('<div>' + html.refinements.fields[1].html + '</div>').find(".value-button").each(function (i, elem) {
+//                        make[i] = {
+//                            value: $(this).data('selected-value'),
+//                            text: $(this).data('selected-display-name'),
+//                            count: $(this).find('span').text(),
+//                        }
+//                    });
+//                    $('<div>' + html.refinements.fields[2].html + '</div>').find(".value-button").each(function (i, elem) {
+//                        model[i] = {
+//                            value: $(this).data('selected-value'),
+//                            text: $(this).data('selected-display-name'),
+//                            count: $(this).find('span').text(),
+//                        }
+//                    });
+//                    $('<div>' + html.refinements.fields[3].html + '</div>').find(".value-button").each(function (i, elem) {
+//                        aggregatedTrim[i] = {
+//                            value: $(this).data('selected-value'),
+//                            text: $(this).data('selected-display-name'),
+//                            count: $(this).find('span').text(),
+//                        }
+//                    });
+//                    filters = {
+//                        radius: radius,
+//                        make: make,
+//                        model: model,
+//                        aggregatedTrim: aggregatedTrim
+//                    }
+//                    data = {
+//                        filters: filters,
+//                        count: html.refinements.count,
+//                        cars: cars
+//                    }
+//                    res.end(JSON.stringify(data));
+//                }
+//            }
+//            , (error) => console.log(err));
+//})
 app.get('/auto-search-results', async function (req, res, next) {
     var q = url.parse(req.url, true).query;
+    var searchPanelParameters = {
+        "Doors": [],
+        "Seats": [],
+        "SafetyRatings": [],
+        "SelectedTopSpeed": null,
+        "SelectedPower": null,
+        "SelectedAcceleration": null,
+        "SelectedEngineSize": null,
+        "BodyStyles": [],
+        "MakeModels": [],
+        "FuelTypes": [],
+        "Transmissions": [],
+        "Colours": [],
+        "IsPaymentSearch": false,
+        "IsReduced": false,
+        "IsHot": false,
+        "IsRecentlyAdded": false,
+        "IsRecommendedSearch": true,
+        "VoucherEnabled": false,
+        "IsGroupStock": false,
+        "PartExAvailable": false,
+        "IsPriceAndGo": false,
+        "IsPreReg": false,
+        "IsExDemo": false,
+        "ExcludeExFleet": false,
+        "ExcludeExHire": false,
+        "Keywords": [],
+        "SelectedInsuranceGroup": null,
+        "SelectedFuelEfficiency": null,
+        "SelectedCostAnnualTax": null,
+        "SelectedCO2Emission": null,
+        "SelectedTowingBrakedMax": null,
+        "SelectedTowingUnbrakedMax": null,
+        "SelectedAdvertType": "*",
+        "SelectedTankRange": null,
+        "DealerId": 0,
+        "Age": -1,
+        "Mileage": -1,
+        "MinPrice": -1,
+        "MaxPrice": -1,
+        "MinPaymentMonthlyCost": -1,
+        "MaxPaymentMonthlyCost": -1,
+        "PaymentTerm": 60,
+        "PaymentMileage": 10000,
+        "PaymentDeposit": 1000,
+        "SelectedSoldStatus": "both",
+        "SelectedBatteryRangeMiles": null,
+        "SelectedBatteryFastChargeMinutes": null,
+        "BatteryIsLeased": false,
+        "BatteryIsWarrantyWhenNew": false,
+        "ExcludeImports": false,
+        "ExcludeHistoryCatNCatD": false,
+        "ExcludeHistoryCatSCatC": false,
+        "ExcludedVehicles": [],
+        "Type": 1,
+        "PostCode": "N111NP",
+        "Distance": 1000,
+        "PaginationCurrentPage": 1,
+        "SortOrder": 0,
+        "DealerGroupId": 0
+    }
     var path = 'https://www.autotrader.co.uk/results-car-search?radius=' + q.radius + '&make=' + q.make + '&model=' + q.model + '&aggregatedTrim=' + q.aggregatedTrim;
     axios.get(path)
             .then((response) => {
                 if (response.status === 200) {
                     const html = response.data;
                     const $ = cheerio.load(html);
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    var cars = {}, filters = {}, radius = {}, make = {}, model = {}, model_variant = {}, aggregatedTrim = {}
-                    $(html.html).find('li.search-page__result').each(function (i, elem) {
-                        cars[i] = {
-                            product_id: $(this).attr('id'),
-                            name: $(this).find('h2').text().trim(),
-                            image: $(this).find(".listing-main-image .js-click-handler img").attr('src'),
-                            listings: $(this).find(".listing-key-specs").html(),
-                            title: $(this).find("p.listing-attention-grabber").text(),
-                            description: $(this).find("p").text(),
-                            price: $(this).find(".vehicle-price").text(),
-                        }
-                    });
-                    $('<div>' + html.refinements.fields[0].html + '</div>').find(".value-button").each(function (i, elem) {
-                        radius[i] = {
-                            value: $(this).data('selected-value'),
-                            text: $(this).data('selected-display-name'),
-                        }
-                    });
-                    $('<div>' + html.refinements.fields[1].html + '</div>').find(".value-button").each(function (i, elem) {
-                        make[i] = {
-                            value: $(this).data('selected-value'),
-                            text: $(this).data('selected-display-name'),
-                            count: $(this).find('span').text(),
-                        }
-                    });
-                    $('<div>' + html.refinements.fields[2].html + '</div>').find(".value-button").each(function (i, elem) {
-                        model[i] = {
-                            value: $(this).data('selected-value'),
-                            text: $(this).data('selected-display-name'),
-                            count: $(this).find('span').text(),
-                        }
-                    });
-                    $('<div>' + html.refinements.fields[3].html + '</div>').find(".value-button").each(function (i, elem) {
-                        aggregatedTrim[i] = {
-                            value: $(this).data('selected-value'),
-                            text: $(this).data('selected-display-name'),
-                            count: $(this).find('span').text(),
-                        }
-                    });
-                    filters = {
-                        radius: radius,
-                        make: make,
-                        model: model,
-                        aggregatedTrim: aggregatedTrim
-                    }
-                    data = {
-                        filters: filters,
-                        count: html.refinements.count,
-                        cars: cars
-                    }
-                    res.end(JSON.stringify(data));
+                    var url = 'https://www.motors.co.uk/search/car/updatesearchpanel';
+                    axios.post(url, searchPanelParameters)
+                            .then((response2) => {
+                                const html2 = response2.data;
+                                var cars = {}, filters = {}, radius = {}, make = {}, model = {}, model_variant = {}, aggregatedTrim = {}
+                                $(html.html).find('li.search-page__result').each(function (i, elem) {
+                                    cars[i] = {
+                                        product_id: $(this).attr('id'),
+                                        name: $(this).find('h2').text().trim(),
+                                        image: $(this).find(".listing-main-image .js-click-handler img").attr('src'),
+                                        listings: $(this).find(".listing-key-specs").html(),
+                                        title: $(this).find("p.listing-attention-grabber").text(),
+                                        description: $(this).find("p").text(),
+                                        price: $(this).find(".vehicle-price").text(),
+                                    }
+                                });
+                                $('<div>' + html.refinements.fields[0].html + '</div>').find(".value-button").each(function (i, elem) {
+                                    radius[i] = {
+                                        value: $(this).data('selected-value'),
+                                        text: $(this).data('selected-display-name'),
+                                    }
+                                });
+                                $('<div>' + html.refinements.fields[1].html + '</div>').find(".value-button").each(function (i, elem) {
+                                    make[i] = {
+                                        value: $(this).data('selected-value'),
+                                        text: $(this).data('selected-display-name'),
+                                        count: $(this).find('span').text().replace('(', '').replace(')', '')
+                                    }
+                                });
+//                                $(make).each(function (key, value) {
+//                                    console.log(value)
+//                                    console.log(html2.MakeModels.indexOf(value.text))
+//                                })
+                                $('<div>' + html.refinements.fields[2].html + '</div>').find(".value-button").each(function (i, elem) {
+                                    model[i] = {
+                                        value: $(this).data('selected-value'),
+                                        text: $(this).data('selected-display-name'),
+                                        count: $(this).find('span').text(),
+                                    }
+                                });
+                                $('<div>' + html.refinements.fields[3].html + '</div>').find(".value-button").each(function (i, elem) {
+                                    aggregatedTrim[i] = {
+                                        value: $(this).data('selected-value'),
+                                        text: $(this).data('selected-display-name'),
+                                        count: $(this).find('span').text(),
+                                    }
+                                });
+                                filters = {
+                                    radius: radius,
+                                    make: make,
+                                    model: model,
+                                    aggregatedTrim: aggregatedTrim
+                                }
+                                data = {
+                                    filters: filters,
+                                    html1: html.refinements,
+                                    html2: html2.MakeModels,
+                                    count: parseInt(html.refinements.count.replace(' cars found', '').replace(/,/g, '')) + parseInt(html2.RecordCount),
+                                    cars: cars
+                                }
+                                res.writeHead(200, {'Content-Type': 'application/json'});
+                                res.end(JSON.stringify(data));
+                            }, (error) => console.log(error));
                 }
             }
             , (error) => console.log(err));
@@ -212,9 +349,9 @@ app.get('/mot-search', async function (req, res, next) {
     }
     axios.post(url, searchPanelParameters)
             .then((response) => {
-//                console.log(response.config.data)
+                console.log(response.data)
                 res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(response.config));
+                res.send(JSON.stringify(response.data));
             }, (error) => console.log(error));
 })
 app.get('/product_search', async function (req, res, next) {
